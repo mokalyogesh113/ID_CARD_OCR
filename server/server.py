85a8f3994acb4be6ca5250bf55dbb3170c222e8f
yogesh.m <mokalyogesh113.dev-33@gmail.com>
2024-04-30 13:22:56 +0530

    Changes

--- server/server.py
+++ server/server.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-<<<<<<< HEAD
-=======
-from waitress import serve
-
->>>>>>> 716db55d09faf6ac26c13863fba8d68193926956
+# from waitress import serve
 import io
 from PIL import Image 
 import numpy as np
@@ -63,8 +59,5 @@ def aadhar_extract_data():
 
 
 if __name__ == '__main__': 
-<<<<<<< HEAD
     app.run(debug=True,port = 5005)
-=======
     # serve(app , host ="0.0.0.0", port = 5000,url_prefix = "/flask-server")
->>>>>>> 716db55d09faf6ac26c13863fba8d68193926956
